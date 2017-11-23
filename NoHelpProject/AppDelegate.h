383572352d1d4e00209c3fa5b281d717005ad970
Tiago Vinagre <tiagovinagre@air-de-tiago.lan>
2017-11-23 10:04:30 +0000

    Initial Commit

---
+++ NoHelpProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoHelpProject
+//
+//  Created by Tiago Vinagre on 23/11/17.
+//  Copyright © 2017 Tiago Vinagre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NoHelpProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NoHelpProject
+//
+//  Created by Tiago Vinagre on 23/11/17.
+//  Copyright © 2017 Tiago Vinagre. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
